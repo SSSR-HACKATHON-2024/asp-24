1ba417ac209b12e6dc76d3e1e18b28700d7c81f9
Александр Смирнов <alesmirnov@astralinux.ru>
2024-11-24 13:22:05 +0300

    Исправление скрипта деплоя

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
 sudo apt install nmap libqt5sql5-psql python3-psycopg2 python3-psutil python3-requests
-sudo mkdir /opt/asp-24
+sudo mkdir -p /opt/asp-24
 sudo cp -r ./* /opt/asp-24
 echo Ready to Roll! Type /opt/asp-24/asp-24s.py -h for help!
